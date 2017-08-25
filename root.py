23891b0652b9b0161fa182611daa563797df7506
Meghana212 <31281556+Meghana212@users.noreply.github.com>
2017-08-25 19:45:22 +0530

    Update root.py

--- root.py
+++ root.py
@@ -1,21 +1,26 @@
+import cmath
 import math
-def root():
-    a =  input("Enter the value of a:")
-    b =  input("Enter the value of b:")
-    c =  input("Enter the value of c:")
+def root(a,b,c):
     D = b*b - 4*a*c
     if D>0:
-       print("The roots are real.")
        root1 = (-b + math.sqrt(D))/2.0*a
        root2 = (-b - math.sqrt(D))/2.0*a
-       print "The roots are : " ,root1,root2
+       return (root1,root2)
     elif D==0:
-       print("The roots are equal.")
        root1 = root2 = -b/2.0*a
-       print "The roots are : Root 1 = Root 2 = " ,root1
+       return (root1,root2)
     else:
-       print("The roots are imaginary.")
        realp = -b/(2.0*a)
-       imgp = math.sqrt(math.fabs(D))/(2.0*a)
-       print "The roots are : {0} + {1}i , {0} - {1}i".format(realp, imgp)
-root()
+       imgp = cmath.sqrt(math.fabs(D))/(2.0*a)
+       root1 = complex(realp,imgp)
+       root2 = complex(realp,-imgp)
+       return (root1,root2)
+
+a =  input("Enter the value of a:")
+b =  input("Enter the value of b:")
+c =  input("Enter the value of c:")
+root(a,b,c)
+root1,root2 = root(a,b,c)
+print "The roots are : " ,root1,root2
+
+
