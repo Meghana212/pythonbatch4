bff945eeaed5a30d23a5697f5473f4f9cae68b94
Meghana212 <31281556+Meghana212@users.noreply.github.com>
2017-10-11 10:19:23 +0530

    Update class order.py

--- class order.py
+++ class order.py
@@ -8,18 +8,23 @@ class menu:
                         print("{0} - Rs. {1}".format(key, value))
         def add(self,s,n):
                 self.Menu[s] = n
-
+class keyerror(Exception):
+        pass
 class order:
         def __init__(self):
                 self.Ord={}
-        def add(self,x,y):
-                self.Ord[x] = y
+        def add(self,x,y):                
+                try:
+                        if x in y.Menu:
+                                self.Ord[x] = y.Menu[x]
+                        else:
+                                raise keyerror
+                except keyerror:
+                        print '\nItem is not in menu.order another item.\n'
         def show(self):
                 print '\nOrdered items:'
                 for key,value in self.Ord.items():
                         print("{0} - Rs.{1}".format(key,value))
-class keyerror(Exception):
-        pass
                         
 c=menu()
 c.add("plain dosa",25)
@@ -32,25 +37,17 @@ while (1):
         a = input("\n(1)Display menu.\n(2)Order item.\n(3)Add item to the menu.\n(4)Exit.\nEnter your choice : ")
         if a==1:
                 c.show()
-       elif a==2:
+        elif a==2:
                 o=order()
                 while(1):
                         a = raw_input("\nEnter the item to be ordered:")
-                        try:
-                                if a in c.Menu:
-                                        o.add(a,c.Menu[a])
-                                        b=raw_input("Do you want to order more items(yes/no)?")
-                                        if b=='yes':
-                                                continue
-                                        else:
-                                                o.show()
-                                                break
-                                else:
-                                        raise keyerror
-
-                        except keyerror:
-                                print 'Item is not in menu.order another item.'
-                                
+                        o.add(a,c)
+                        b=raw_input("Do you want to order more items(yes/no)?")
+                        if b=='yes':
+                                continue
+                        else:
+                                o.show()
+                                break                               
         elif a==3:
                 c.add(raw_input("Enter the item to be added : "),input("Enter the price of the item : "))
         elif a==4:
