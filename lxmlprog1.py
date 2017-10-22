41a812568d144ef22a01157771a74d9900606fd9
Meghana212 <31281556+Meghana212@users.noreply.github.com>
2017-10-22 10:41:11 +0530

    lxml

---
+++ lxmlprog1.py
@@ -0,0 +1,13 @@
+from lxml import etree
+tree = etree.parse('plant_catalog.xml')
+root = tree.getroot()
+x = "Aquilegia canadensis"
+def find_common_name(tree,root,x):
+    for element in root:
+        for alltags in element.findall('.//'):
+            if alltags.tag=="BOTANICAL" and alltags.text==x:
+                name = element.find("COMMON").text
+                return name
+
+name = find_common_name(tree,root,x)
+print 'The common name is : ',name
